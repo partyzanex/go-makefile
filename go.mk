c704f3d31327870f30b3931a7a50640239b4a36d
Дмитрий Золотарев <partyzan65@gmail.com>
2022-10-11 13:07:53 +0300

    fixed go.mk

--- go.mk
+++ go.mk
@@ -28,3 +28,55 @@ golangci-lint-install: bin-default
 .PHONY: lint-default
 lint-default: golangci-lint-install
 	@$(GOLANGCI_LINT_BIN) run
+
+GOOSE_VERSION :=v3.6.1
+GOOSE_BIN=$(LOCAL_BIN)/goose
+
+.PHONY: goose-install
+goose-install:
+	@go-install -v -e github.com/pressly/goose/v3/cmd/goose@$(GOOSE_VERSION)
+
+PG_WAIT_VERSION :=v0.1.3
+PG_WAIT_BIN=$(LOCAL_BIN)/pg-wait
+
+.PHONY: pg-wait-install
+pg-wait-install:
+	@go-install -v -e github.com/partyzanex/pg-wait/cmd/pg-wait@$(PG_WAIT_VERSION)
+
+.PHONY: pg-wait
+pg-wait: pg-wait-install
+ifdef POSTGRES_DSN
+	@$(PG_WAIT_BIN) -d $(POSTGRES_DSN) -v
+else
+	@echo "POSTGRES_DSN is undefined!"
+endif
+
+.PHONY: goose-up
+goose-up: goose-install
+ifdef POSTGRES_DSN
+	@$(GOOSE_BIN) -dir $(CURDIR)/migrations postgres $(POSTGRES_DSN) up
+else
+	@echo "POSTGRES_DSN is undefined!"
+endif
+
+.PHONY: goose-down
+goose-down: goose-install
+ifdef POSTGRES_DSN
+	@$(GOOSE_BIN) -dir $(CURDIR)/migrations postgres $(POSTGRES_DSN) down
+else
+	@echo "POSTGRES_DSN is undefined!"
+endif
+
+GRPCURL_VERSION := v1.7.0
+GRPCURL_BIN := $(LOCAL_BIN)/grpcurl
+
+.PHONY: grpcurl-install
+grpcurl-install:
+	@go-install -v -e github.com/fullstorydev/grpcurl/cmd/grpcurl@$(GRPCURL_VERSION) $(GRPCURL_BIN)
+
+MOCKGEN_VERSION := v1.6.1
+MOCKGEN_BIN := $(LOCAL_BIN)/mockgen
+
+.PHONY: mockgen-install
+mockgen-install:
+	@go-install -v -e github.com/golang/mock/mockgen@$(MOCKGEN_VERSION) $(MOCKGEN_BIN)
