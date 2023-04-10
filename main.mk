31c2c530f6282c001b8d7ba7c3b9f7f534c3c6ed
dmitry <partyzan-lipetsk@yandex.ru>
2023-04-10 21:01:16 +0300

    added .gitignore

--- main.mk
+++ main.mk
@@ -5,5 +5,10 @@ MAKE_PATH=$(LOCAL_BIN):/bin:/usr/bin:/usr/local/bin
 bin-default:
 	@mkdir -p $(LOCAL_BIN)
 
+.gitignore:
+	@tmpdir=$$(mktemp -d) && \
+	git clone --depth 1 --single-branch https://github.com/partyzanex/go-makefile.git $$tmpdir && \
+	cp $$tmpdir/template.gitignore $(CURDIR)/.gitignore
+
 %:  %-default
 	@  true
