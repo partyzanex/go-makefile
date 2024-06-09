dc5752411232cdc7b8d44ace82109475d0a61d5b
dmitry <partyzan65@gmail.com>
2024-06-09 03:23:04 +0300

    updated protoc, removed buf

--- proto.mk
+++ proto.mk
@@ -1,4 +1,4 @@
-ifndef GO_INSTALL_VERSION
+ifndef GOLANGCI_LINT_VERSION
 include go.mk
 endif
 
@@ -8,20 +8,37 @@ go.mk:
 	cp $$tmpdir/go.mk $(CURDIR)/go.mk
 
 # versions
-PROTOC_GRPC_GATEWAY_VERSION=v2.19.0
-PROTOC_GEN_GO_VERSION=v1.32.0
-PROTOC_GEN_GO_GRPC_VERSION=v1.3.0
-PROTOC_VERSION := 25.2
+PROTOC_GRPC_GATEWAY_VERSION := latest
+PROTOC_GEN_GO_VERSION := latest
+PROTOC_GEN_GO_GRPC_VERSION := latest
+PROTOC_VERSION := 27.1
+
 # paths
+PROTOC_GRPC_GATEWAY_BIN=$(LOCAL_BIN)/protoc-gen-grpc-gateway
+PROTOC_GEN_OPENAPIV2_BIN=$(LOCAL_BIN)/protoc-gen-openapiv2
+PROTOC_GEN_GO_GRPC_BIN=$(LOCAL_BIN)/protoc-gen-go-grpc
+PROTOC_GEN_GO_BIN=$(LOCAL_BIN)/protoc-gen-go
 PROTOC_BIN=$(LOCAL_BIN)/protoc
 
-.PHONY: protoc-install
-protoc-install: bin-default
+.PHONY: protoc-install protoc-grpc-gateway-install protoc-gen-openapiv2-install protoc-gen-go-grpc-install protoc-gen-go-install
+
+protoc-grpc-gateway-install: bin
+	$(call go_build_install,github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway,$(PROTOC_GRPC_GATEWAY_VERSION),$(PROTOC_GRPC_GATEWAY_BIN))
+
+protoc-gen-openapiv2-install: bin
+	$(call go_build_install,github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2,$(PROTOC_GRPC_GATEWAY_VERSION),$(PROTOC_GEN_OPENAPIV2_BIN))
+
+protoc-gen-go-grpc-install: bin
+	$(call go_build_install,google.golang.org/grpc/cmd/protoc-gen-go-grpc,$(PROTOC_GEN_GO_GRPC_VERSION),$(PROTOC_GEN_GO_GRPC_BIN))
+
+protoc-gen-go-install: bin
+	$(call go_build_install,google.golang.org/protobuf/cmd/protoc-gen-go,$(PROTOC_GEN_GO_VERSION),$(PROTOC_GEN_GO_BIN))
+
+protoc-install: protoc-grpc-gateway-install protoc-gen-openapiv2-install protoc-gen-go-grpc-install protoc-gen-go-install
 ifeq ($(wildcard $(PROTOC_BIN)),)
 ifeq ($(shell uname -s), Darwin)
-	@tmp=$$(mktemp -d) && zip=$$tmp/protoc.zip && \
-	platform=$$(uname -s | sed 's/Darwin/osx/') && arch=$$(uname -m) && \
-	curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOC_VERSION)/protoc-$(PROTOC_VERSION)-$$platform-$$arch.zip -o $$zip && \
+	tmp=$$(mktemp -d) && zip=$$tmp/protoc.zip && \
+	curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOC_VERSION)/protoc-$(PROTOC_VERSION)-osx-universal_binary.zip -o $$zip && \
 	cd $$tmp && unzip $$zip &> /dev/null && cp $$tmp/bin/protoc $(PROTOC_BIN)
 endif
 ifeq ($(shell uname -s), Linux)
@@ -34,40 +51,6 @@ ifeq ($(OS),Windows_NT)
 endif
 	@chmod +x $(PROTOC_BIN) && echo "protoc installed!"
 endif
-	@go-install -v -e github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@$(PROTOC_GRPC_GATEWAY_VERSION)
-	@go-install -v -e github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@$(PROTOC_GRPC_GATEWAY_VERSION)
-	@go-install -v -e google.golang.org/protobuf/cmd/protoc-gen-go@$(PROTOC_GEN_GO_VERSION)
-	@go-install -v -e google.golang.org/grpc/cmd/protoc-gen-go-grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
-
-# source URLs
-BUF_SOURCE_URL := github.com/bufbuild/buf/cmd/buf
-BUF_LINT_SOURCE_URL := github.com/bufbuild/buf/cmd/protoc-gen-buf-breaking
-BUF_BREAKING_SOURCE_URL := github.com/bufbuild/buf/cmd/protoc-gen-buf-lint
-# paths
-BUF_BIN=$(LOCAL_BIN)/buf
-BUF_LINT_BIN=$(LOCAL_BIN)/protoc-gen-buf-lint
-BUF_BREAKING_BIN=$(LOCAL_BIN)/protoc-gen-buf-breaking
-BUF_TEMPLATE_PATH=$(CURDIR)/buf.yaml
-BUF_GEN_TEMPLATE_PATH=$(CURDIR)/buf.gen.yaml
-# versions
-BUF_VERSION := v1.8.0
-
-.PHONY: buf.yaml
-buf.yaml:
-	@[ ! -f $(BUF_TEMPLATE_PATH) ] || exit 0 && \
-	tmpdir=$$(mktemp -d) && \
-	git clone --depth 1 --single-branch https://github.com/partyzanex/go-makefile.git $$tmpdir && \
-	cp $$tmpdir/buf.template.yaml $(BUF_TEMPLATE_PATH)
-	@[ ! -f $(BUF_GEN_TEMPLATE_PATH) ] || exit 0 && \
-	tmpdir=$$(mktemp -d) && \
-	git clone --depth 1 --single-branch https://github.com/partyzanex/go-makefile.git $$tmpdir && \
-	cp $$tmpdir/buf.gen.template.yaml $(BUF_GEN_TEMPLATE_PATH)
-
-.PHONY: buf-install
-buf-install: bin-default buf.yaml
-	@go-install -v -e $(BUF_SOURCE_URL)@$(BUF_VERSION) $(BUF_BIN)
-	@go-install -v -e $(BUF_LINT_SOURCE_URL)@$(BUF_VERSION) $(BUF_LINT_BIN)
-	@go-install -v -e $(BUF_BREAKING_SOURCE_URL)@$(BUF_VERSION) $(BUF_BREAKING_BIN)
 
 PROTO_DIR := $(CURDIR)/proto
 
@@ -77,8 +60,7 @@ ifeq ($(wildcard $(PROTO_DIR)/google),)
 # for osx
 ifeq ($(shell uname -s), Darwin)
 	@tmp=$$(mktemp -d) && zip=$$tmp/protoc.zip && \
-	platform=$$(uname -s | sed 's/Darwin/osx/') && arch=$$(uname -m) && \
-	curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOC_VERSION)/protoc-$(PROTOC_VERSION)-$$platform-$$arch.zip -o $$zip && \
+	curl -k -L https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOC_VERSION)/protoc-$(PROTOC_VERSION)-win64.zip -o $$zip && \
 	cd $$tmp && unzip $$zip &> /dev/null && mkdir -p $(PROTO_DIR) && cp -R $$tmp/include/google $(PROTO_DIR)
 endif
 # for linux
@@ -94,14 +76,24 @@ endif
 	done
 endif
 
-GENERATED_GO_PATH ?= $(CURDIR)/generated/go
+GENERATED_GO_PATH ?= $(CURDIR)/pkg/proto
 
 .PHONY: proto-default
-proto-default: protoc-install buf-install proto-deps
+proto-default: protoc-install proto-deps
 	@rm -rf $(GENERATED_GO_PATH) && mkdir -p $(GENERATED_GO_PATH) && \
-	PATH=$(MAKE_PATH) $(BUF_BIN) generate proto && \
+	PATH=$(MAKE_PATH) $(PROTOC_BIN) \
+		--proto_path=$(PROTO_DIR): \
+		--go_opt=paths=source_relative \
+		--go_out=$(GENERATED_GO_PATH) \
+		--go-grpc_opt=paths=source_relative \
+		--go-grpc_out=$(GENERATED_GO_PATH) \
+		--grpc-gateway_opt=paths=source_relative \
+		--grpc-gateway_out=$(GENERATED_GO_PATH) \
+		--openapiv2_out=$(GENERATED_GO_PATH) \
+		$$(find $(PROTO_DIR) -not \( -path $(PROTO_DIR)/google -prune \) -iname "*.proto")
 	rm -rf $(GENERATED_GO_PATH)/google
 	@for dir in $(GENERATED_GO_PATH)/*/; do \
 	  cd $$dir && \
 	  go mod init && go mod tidy && go mod vendor; \
 	done
+
